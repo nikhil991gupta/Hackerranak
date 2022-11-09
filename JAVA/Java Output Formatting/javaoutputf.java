3a99cfab0fc7a839a955f498fa8bc8e5ec0c3a99
nikhil991gupta <nikhil991gupta@gmail.com>
2022-11-09 17:39:26 +0530

    Hackerrank

---
+++ JAVA/Java Output Formatting/javaoutputf.java
@@ -0,0 +1,29 @@
+  
+
+import java.util.Scanner;
+
+public class javaoutputf {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                System.out.printf("%-15s%03d%n",s1,x);
+                // %s is used for string so,
+// %-15s --> is used for -15 spaces right side for string
+//  same as
+// %d is used for int
+// %03 --> used for 3 spaces left side for int with starting 0
+                    
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
